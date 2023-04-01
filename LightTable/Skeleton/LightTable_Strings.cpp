0bf0ac90d6c41e65b5dcaf36532ae6c53d47c5de
Mos Zheng <47658172+moszheng@users.noreply.github.com>
2023-04-01 20:50:40 +0800

    0401

---
+++ LightTable/Skeleton/LightTable_Strings.cpp
@@ -0,0 +1,23 @@
+#include "LightTable.h"
+
+typedef struct {
+	A_u_long	index;
+	A_char		str[256];
+} TableString;
+
+
+
+TableString		g_strs[StrID_NUMTYPES] = {
+	StrID_NONE,						"",
+	StrID_Name,						"LightTable",
+	StrID_Description,				"An empty (skeletal, if you will) effect sample,\r for your modifying pleasure.\rCopyright 2007 Adobe Systems Incorporated.",
+	StrID_Gain_Param_Name,			"Gain",
+	StrID_Color_Param_Name,			"Color",
+};
+
+
+char	*GetStringPtr(int strNum)
+{
+	return g_strs[strNum].str;
+}
+	
